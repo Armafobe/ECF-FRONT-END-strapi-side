e2452967408a81ece15e4ffc23463e151fd1d0b8
Arthur Berthou <arthur_berthou@hotmail.com>
2022-06-03 13:51:22 +0200

    fix

--- config/admin.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  apiToken: {
-    salt: env('API_TOKEN_SALT', '12345678987654321'),
-  },
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', '98765432123456789'),
-  },
-});
-  
