2def0a082a1bf9835a458903ce9991d00cae3675
Arthur Berthou <arthur_berthou@hotmail.com>
2022-06-03 14:04:16 +0200

    Create admin.js

---
+++ config/admin.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'wQHTDU1GaTi4YhVWxdFC8w=='),
+  },
+  auth: {
+    secret: env('ADMIN_JWT_SECRET', 'uTEK+1KkTFyO71kWT7saOA=='),
+  },
+});
+  
