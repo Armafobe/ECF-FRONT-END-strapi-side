ff4fecd8fb18571aee7ebbe41d1cea47040cd697
Arthur Berthou <arthur_berthou@hotmail.com>
2022-06-07 14:15:58 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -8,10 +8,13 @@ module.exports = ({ env }) => ({
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "0000"),
-      schema: env("DATABASE_SCHEMA", "public"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
+    debug: false,
   },
 });
 /*defaultConnection: "default",
