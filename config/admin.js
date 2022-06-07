c4d2bdd9486b5c78d98e8ccfd06fbcc4fe0ac55b
Arthur Berthou <arthur_berthou@hotmail.com>
2022-06-07 21:02:36 +0200

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
-/*module.exports = ({ env }) => ({
+module.exports = ({ env }) => ({
   apiToken: {
-    salt: env('API_TOKEN_SALT', '8eab61a8-17fe-4ebf-8af6-7cf78888b16e'),
+    salt: env('API_TOKEN_SALT', 'dqz54384f8348fsf4s39fs2fs3ef51s8e4'),
   },
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'd1838bb10cede4205ad9420ae59ed191'),
   },
-});*/
+});
