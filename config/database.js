ed5d9929bc97a94efeea6e437d3d0fd73177ac8f
Arthur Berthou <arthur_berthou@hotmail.com>
2022-06-08 12:54:49 +0200

    database

--- config/database.js
+++ config/database.js
@@ -2,18 +2,17 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "sqlite",
-        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "bank"),
+      username: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "0000"),
+      schema: env("DATABASE_SCHEMA", "public"),
     },
-  },
+  }
 });
   /*if (process.env.NODE_ENV === "production") {
     return {
